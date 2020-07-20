305d15e01e5ce8c9f6476ef1a31c77277c0fe531
ahmedeldessouki <nemo@DESKTOP-AMR3GO5.localdomain>
2020-07-20 22:48:24 +0300

    prettier

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from "react";
-import Header from "./components/Header/Header";
-import HomePage from "./components/HomePage/HomePage";
+import React from 'react';
+import Header from './components/Header/Header';
+import HomePage from './components/HomePage/HomePage';
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <HomePage />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <HomePage />
+        </div>
+    );
 }
 
 export default App;
